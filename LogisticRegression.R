364e773cb4dc4c331173de4059b8ca10d6a46470
chouayaSaif <saif.chouaya@tbs.u-tunis.tn>
2025-02-12 16:36:37 +0100

    logisitc regression: predicting if a student get admitted or not v2

--- LogisticRegression.R
+++ LogisticRegression.R
@@ -47,3 +47,29 @@ head(p1)
 head(train)
 
 # result: applicant 1: get 27.7% chance of being admitted: low --> not admitted (admit = 0)
+
+
+# Misclassification error - train data
+pred1 <- ifelse(p1>0.5, 1, 0) # a vectorized conditional function, checks if each element of p1 is greater than 0.5. If it is, the condition evaluates to TRUE  and retuns 1; otherwise, it evaluates to FALSE and returns 0
+pred1
+tab1 <- table(Pedicted = pred1, Actual = train$admit) # confusion matrix
+tab1
+# --->Misclassification: 69 applicant who are actually admitted, but model predicts to eb not addmitted
+
+1 - sum(diag(tab1))/sum(tab1) # sum(204+26) correctly classified
+# ---> Misclassifiation error = 24.8%
+
+# Misclassification error - test data
+p2 <- predict(model, test, type = 'response')
+pred2 <- ifelse(p2>0.5, 1, 0)
+tab2 <- table(Pedicted = pred2, Actual = test$admit)
+tab2
+1 - sum(diag(tab2))/sum(tab2)
+# ---> Slightly higher: 34.04%
+
+
+
+# Goodness-of-fit test
+with(model, pchisq(null.deviance -deviance, df.null-df.residual, lower.tail = F))
+# we get a p-value = 4.5e-06
+# confidence level high: this model is statiscally significant
