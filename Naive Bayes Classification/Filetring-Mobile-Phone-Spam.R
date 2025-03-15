dfc8f60e53f9e9c81baba96ea7f2bf791052e9a5
chouayaSaif <saif.chouaya@tbs.u-tunis.tn>
2025-03-15 15:37:16 +0100

    filtering mobile phone spam with tthe naive Bayes algorithm

---
+++ Naive Bayes Classification/Filetring-Mobile-Phone-Spam.R
@@ -0,0 +1,7 @@
+##############################################################
+# Classification using Naive Bayes                           #
+# Machine Learning with R by Brett Lantz                     #
+# Filtering mobile phone spam with the naive Bayes algorithm #
+##############################################################
+
+
