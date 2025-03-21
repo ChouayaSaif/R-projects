b624d95e49b0f82feae446655641917761b30a60
chouayaSaif <saif.chouaya@tbs.u-tunis.tn>
2025-03-21 16:11:40 +0100

    time series classification v2

--- TimeSeriesAnalysis/Time Series Classification/classifying_synthetic_control_databases.R
+++ TimeSeriesAnalysis/Time Series Classification/classifying_synthetic_control_databases.R
@@ -27,9 +27,13 @@ newdata <- data.frame(data, pattern100)
 str(newdata)
 
 # Classification with decision tree
+install.packages("party")
 library(party)
-tree <- ctree(pattern100~., newdata)
+newdata[] <- lapply(newdata, function(x) if (is.character(x)) as.factor(x) else x)
+
+tree <- ctree(pattern100~., newdata) # ctree: classification tree
 
 # Classification performance
 tab <- table(Predicted = predict(tree, newdata), Actual = newdata$pattern100)
-sum(diag(tab))/sum(tab)
+sum(diag(tab))/sum(tab) # calculate accuracy of classification
+# --> this classification is 95% accurate
