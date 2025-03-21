8446004ee87340dc340df857326acba3ad30f00d
chouayaSaif <saif.chouaya@tbs.u-tunis.tn>
2025-03-21 20:14:44 +0100

    working with neural networks for a binary categorical output and predicting if a student is admit

---
+++ Neural networks/NN-to-BinaryOutput.R
@@ -0,0 +1,18 @@
+data <- read.csv('https://raw.githubusercontent.com/bkrai/R-files-from-YouTube/main/binary.csv')
+str(data)
+tail(data)
+
+hist(data$gre)
+
+# Min-Max Normalization
+data$gre <- (data$gre - min(data$gre))/(max(data$gre) - min(data$gre))
+data$gpa <- (data$gpa - min(data$gpa))/(max(data$gpa) - min(data$gpa))
+data$rank <- (data$rank - min(data$rank))/(max(data$rank)-min(data$rank))
+
+hist(data$gre)
+
+# Data Partition
+set.seed(222)
+ind <- sample(2, nrow(data), replace = TRUE, prob = c(0.7, 0.3))
+training <- data[ind==1,]
+testing <- data[ind==2,]
