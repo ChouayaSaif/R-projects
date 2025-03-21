4b5b3eadf476024899a5c21113a40c29950baac5
chouayaSaif <saif.chouaya@tbs.u-tunis.tn>
2025-03-21 17:00:28 +0100

    forecasting Tom Brady's Wikipedia data using facebook Prophet package v2

--- TimeSeriesAnalysis/Tom Brady Wikipedia Data/Facebbok-Prophet-Forecasting.R
+++ TimeSeriesAnalysis/Tom Brady Wikipedia Data/Facebbok-Prophet-Forecasting.R
@@ -14,7 +14,32 @@ data <- wp_trend(page = "Tom_Brady",
 install.packages("ggplot2")
 library(ggplot2)
 qplot(date, views, data = data)
+summary(data)
 
 
+# Handling missing data
+data$views[data$views == 0] <- NA  # Convert zeros to NA
+ds <- data$date # ds: datastamp
+y <- log(data$views) # log transformation
+y
+df <- data.frame(ds,y)
+df
+qplot(ds, y, data=df)
 
 
+# Forecasting with Facebook's Prophet
+install.packages("prophet")
+library(prophet)
+m <- prophet(df)
+m
+
+# Prediction
+future <- make_future_dataframe(m, periods = 365)
+tail(future)
+forecast <- predict(m, future)
+tail(forecast[c('ds', 'yhat','yhat_lower',"yhat_upper")])
+exp(8.334196) # taking expo of predicted yhat values to cancel out log effect
+
+# Plot forecast
+plot(m, forecast)
+prophet_plot_components(m, forecast)
