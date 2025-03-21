1a37ed4f1d3a4ceff455b8f9a37b072da6312a2c
chouayaSaif <saif.chouaya@tbs.u-tunis.tn>
2025-03-21 15:59:14 +0100

    time series classification v1

---
+++ TimeSeriesAnalysis/Time Series Classification/classifying_synthetic_control_databases.R
@@ -0,0 +1,35 @@
+###############################
+# Time series classification  #
+###############################
+
+# Data 
+# http://kdd.ics.uci.edu/databases/synthetic_control/synthetic_control.html
+data <- read.table(file.chogose(), header = F, sep = "")
+str(data)
+View(data)
+plot(data[,60], type = 'l')
+j <- c(5, 105, 205, 305, 405, 505)
+sample <- t(data[j,])
+plot.ts(sample,
+        main = "Time-series Plot",
+        col = 'blue',
+        type = 'b')
+
+
+# Data preparation
+pattern100 <- c(rep('Normal', 100),
+                rep('Cyclic', 100),
+                rep('Increasing trend', 100),
+                rep('Decreasing trend', 100),
+                rep('Upward shift', 100),
+                rep('Downward shift', 100))
+newdata <- data.frame(data, pattern100)
+str(newdata)
+
+# Classification with decision tree
+library(party)
+tree <- ctree(pattern100~., newdata)
+
+# Classification performance
+tab <- table(Predicted = predict(tree, newdata), Actual = newdata$pattern100)
+sum(diag(tab))/sum(tab)
